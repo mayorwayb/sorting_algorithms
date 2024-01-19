61b89ee80657c13c7069fe05922093d5605c21dc
mayorwayb <mayorwayb@gmail.com>
2024-01-19 13:35:37 -0800

    100-shell_sort.c

--- sort.h
+++ sort.h
@@ -37,5 +37,6 @@ void selection_sort(int *array, size_t size);
 int partition(int *array, int low, int high, size_t size);
 void quicksort(int *array, int low, int high, size_t size);
 void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
 
 #endif
